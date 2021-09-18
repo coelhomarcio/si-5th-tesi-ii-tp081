93e92ec25e76c717af93ce9e11952452b81d97fc
Marcio Coelho <marciocoelho@gmail.com>
2021-09-18 02:07:07 -0300

    Updated app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }           from "@angular/core";
+import { BrowserModule }      from "@angular/platform-browser";
+import { HttpClientModule }   from "@angular/common/http";
+import { registerLocaleData } from "@angular/common";
+import localePt               from "@angular/common/locales/pt";
+import localeEn               from "@angular/common/locales/en";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+
+import { AppComponent }    from "./app.component";
+import { HeaderComponent } from "./component/header/header.component";
+import { HomeComponent }   from "./component/home/home.component";
+
+import { BitcoinRateService } from "./service/bitcoin-rate.service";
+import { TimerService }       from "./service/timer.service";
+
+registerLocaleData(localePt);
+registerLocaleData(localeEn);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
-  imports: [
+  imports:      [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers:    [
+    BitcoinRateService,
+    TimerService
+  ],
+  bootstrap:    [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
